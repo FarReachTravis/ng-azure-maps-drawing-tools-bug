57f003f7ce157a3b1ad29dd755800d01fd8c0a9c
Travis Rood <travis@farreachinc.com>
2021-06-16 16:03:59 -0600

    initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import * as atlas from 'azure-maps-control';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-azure-maps';
+
+  public dataSource: atlas.source.DataSource;
+  public strokeColor = '#4288f7';
+  public strokeWidth = 1;
+  public radius = 7;
+  public color = "DodgerBlue";
+  public center = [-92.0076434351666, 42.31463750062173];
+  public zoom = 6;
+  public mapStyle = 'grayscale_light';
+
+  mapReady() {
+    this.dataSource = new atlas.source.DataSource('source');
+    const point1 = new atlas.Shape(new atlas.data.Point([-92.0076434351666, 42.31463750062173]));
+    this.dataSource.add([point1]);
+    const point2 = new atlas.Shape(new atlas.data.Point([-92.76434351666, 42.463750062173]));
+    this.dataSource.add([point2]);
+    const point3 = new atlas.Shape(new atlas.data.Point([-92.25436, 42.4773]));
+    this.dataSource.add([point3]);
+  }
+
+  drawingStarted(value: any) {
+  }
+  drawingChanged(value: any) {
+  }
+  drawingChanging(value: any) {
+  }
+  drawingComplete(value: any) {
+  }
+  drawingModeChanged(value: any) {
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AzureMapsModule } from 'ng-azure-maps';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AzureMapsModule.forRoot({
+      authOptions: environment.authOptions
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
